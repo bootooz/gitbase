e0fa68773137bdf5d7cdb41999260a82d6bd8632
bootooz <disel0794@mail.ru>
2016-11-23 02:01:09 +0500

    commit for new brench

--- LICENCE.php
+++ LICENCE.php
@@ -3,8 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>LICENCE</title>
+	<link rel="stylesheet" href="app/css/main.css">
 </head>
 <body>
-	<h1>HELLO LICENCE!</h1>
+
+<section id="body">
+	<p>HELLO LICENCE!</p>
+</section>
+
+
+
+
+
+
+
+
+
+
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 </html>
